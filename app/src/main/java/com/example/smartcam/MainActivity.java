7f9dfa357b373250262dc64678e57408d8744d5f
Tran Huynh Ngoc <21522384@gm.uit.edu.vn>
2024-05-06 18:06:07 +0700

    connect with firebase (signin, signup)

--- app/src/main/java/com/example/smartcam/MainActivity.java
+++ app/src/main/java/com/example/smartcam/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.smartcam;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,13 +11,24 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
 
+    FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+    FirebaseUser currentUser = firebaseAuth.getCurrentUser();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+       // setContentView(R.layout.activity_main);
+        if(currentUser==null){
+            Toast.makeText(this, "Please sign in", Toast.LENGTH_SHORT).show();
+            setContentView(R.layout.login);
+        }else {
+            setContentView(R.layout.activity_main);
+        }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
--- app/src/main/java/com/example/smartcam/registerpage.java
+++ app/src/main/java/com/example/smartcam/registerpage.java
@@ -8,20 +8,31 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class registerpage extends AppCompatActivity {
 
     TextInputEditText iedtEmail2, iedtPassword2, iedtUsername;
     Button btnsignUp;
     TextView tvsignIn;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registerpage);
+
+        mAuth = FirebaseAuth.getInstance();
+
         iedtUsername = findViewById(R.id.input_username);
         iedtEmail2 = findViewById(R.id.input_email2);
         iedtPassword2 = findViewById(R.id.input_passwd2);
@@ -39,29 +50,46 @@ public class registerpage extends AppCompatActivity {
         btnsignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String email2, password2, username;
-                email2 = iedtEmail2.getText().toString().trim();
-                password2 = iedtPassword2.getText().toString().trim();
-                username = iedtUsername.getText().toString().trim();
-                //kiểm tra kết email2, password2, username không empty
-                if (TextUtils.isEmpty(email2)) {
-                    Toast.makeText(registerpage.this, "Please Enter Email", Toast.LENGTH_SHORT).show();
-                    return;
-                }
-                if (TextUtils.isEmpty(password2)) {
-                    Toast.makeText(registerpage.this, "Please enter password", Toast.LENGTH_SHORT).show();
-                    return;
-                } else if (password2.length() < 7) {
+                register();
+            }
+        });
+    }
+    public void register(){
+        String email2, password2, username;
+        email2 = iedtEmail2.getText().toString().trim();
+        password2 = iedtPassword2.getText().toString().trim();
+        username = iedtUsername.getText().toString().trim();
+        //kiểm tra kết email2, password2, username không empty
+        if (TextUtils.isEmpty(email2)) {
+            Toast.makeText(registerpage.this, "Please Enter Email", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (TextUtils.isEmpty(password2)) {
+            Toast.makeText(registerpage.this, "Please enter password", Toast.LENGTH_SHORT).show();
+            return;
+        } else if (password2.length() < 7) {
 
-                    Toast.makeText(registerpage.this, "Password should greater than 7 digits", Toast.LENGTH_SHORT).show();
+            Toast.makeText(registerpage.this, "Password should greater than 7 digits", Toast.LENGTH_SHORT).show();
+        }
+        if (TextUtils.isEmpty(username)) {
+            Toast.makeText(registerpage.this, "Please enter username", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        mAuth.createUserWithEmailAndPassword(email2, password2).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    //FirebaseUser user = mAuth.getCurrentUser();
+                   // FirebaseDatabase.getInstance().getReference("users")
+                   //         .child(user.getUid()).child("username").setValue(username);
+                    Toast.makeText(getApplicationContext(), "Sign up successfully", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(registerpage.this, log_in.class);
+                    startActivity(intent);
                 }
-                if (TextUtils.isEmpty(username)) {
-                    Toast.makeText(registerpage.this, "Please enter username", Toast.LENGTH_SHORT).show();
-                    return;
+                else {
+                    Toast.makeText(getApplicationContext(), "Sign up failed", Toast.LENGTH_SHORT).show();
                 }
             }
         });
-
     }
-
 }
