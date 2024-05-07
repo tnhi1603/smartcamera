2a3cc563821ce8f35f7d5248d0e3eae92162e177
tnhi1603 <21521236@gm.uit.edu.vn>
2024-05-08 01:53:04 +0700

    update

--- app/src/main/java/com/example/smartcam/log_in.java
+++ app/src/main/java/com/example/smartcam/log_in.java
@@ -1,5 +1,6 @@
 package com.example.smartcam;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -10,17 +11,23 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.Firebase;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class log_in extends AppCompatActivity {
-
     TextInputEditText iedtEmail, iedtPassword;
     Button btn_signIn;
     TextView tv_signUp, tv_reset;
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+        mAuth= FirebaseAuth.getInstance();
 
         iedtEmail = findViewById(R.id.input_email);
         iedtPassword = findViewById(R.id.input_passwd);
@@ -34,7 +41,6 @@ public class log_in extends AppCompatActivity {
                 Intent intent= new Intent(log_in.this,registerpage.class);
                 startActivity(intent);
                 finish();
-
             }
         });
 //        btn_signIn.setOnClickListener(new View.OnClickListener() {
@@ -54,9 +60,44 @@ public class log_in extends AppCompatActivity {
 //            }
 //        });
 
+
+        btn_signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login();
+            }
+        });
+
     }
     public void home(View view){
         Intent intent = new Intent(log_in.this,MainActivity.class);
         startActivity(intent);
+    }
+    public void login(){
+        String email, password;
+        email = iedtEmail.getText().toString().trim();
+        password = iedtPassword.getText().toString().trim();
+
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(log_in.this, "Please Enter Email", Toast.LENGTH_SHORT).show();
+            return;
+            }
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(log_in.this, "Please enter password", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(getApplicationContext(), "Sign in successfully", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(log_in.this, MainActivity.class);
+                    startActivity(intent);
+                }else{
+                    Toast.makeText(getApplicationContext(), "Sign in failed", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
